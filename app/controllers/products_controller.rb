d2ad3fc255e49efe208ef97e9daf53f4bc4034da
hu.xiaofei <xiaofei.hu@weilinct.com>
2026-01-15 00:20:29 +0800

    feat: product controller CRUD actions.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,58 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: %i[ show edit update destroy ]
+
   def index
+      @products = Product.all
   end
+
+
+
+  def show
+  end
+
+
+  def new
+    @product = Product.new
+  end
+
+
+
+  def create
+    @product = Product.new product_param
+
+    if @product.save
+      redirect_to @product, notice: "商品新增成功"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+ 
+
+  def edit
+  end
+
+  def update
+    if @product.update product_param
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+  private
+
+    def set_product
+      @product = Product.find params[:id]
+    end
+
+    def product_param
+      params.expect(product: %w[name])
+    end
+
+
 end
